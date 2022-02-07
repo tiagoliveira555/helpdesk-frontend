e6510fbd4f8734273f4c914dc2274cf3c2b3b76a
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-02-06 21:10:22 -0300

    rota para chamado criado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -15,7 +15,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) {}
 
   ngOnInit(): void {
-    this.router.navigate(["home"]);
+    this.router.navigate(["chamados"]);
   }
 
   logout() {
